225cca1561db9e58876fdf8556af7fbeea2db59b
“ktym4a” <s19585574@gmail.com>
2020-09-17 21:05:19 +0700

    Header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,14 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
-const Header: React.FC = () => <header>KTYM4a</header>
+const Header: React.FC = () => <HeaderContainer>KTYM4a</HeaderContainer>
+
+const HeaderContainer = styled.header`
+  color: #282c35;
+  font-weight: blod;
+  font-size: 5rem;
+  text-align: center;
+  padding: 20px;
+`
 
 export default Header
