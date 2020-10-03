776124af4893e0c5cec4d329c44ec573b16d76a4
“ktym4a” <s19585574@gmail.com>
2020-10-03 10:52:10 +0700

    不用なファイルを削除

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,18 +48,5 @@ module.exports = {
         },
       },
     },
-    {
-      resolve: `gatsby-plugin-mdx`,
-      options: {
-        extensions: [`.mdx`, `.md`],
-      },
-    },
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-      },
-    }
   ],
 }
