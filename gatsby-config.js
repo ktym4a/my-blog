e12e615ebeb674005f4d23cce3edd00d934a0483
“ktym4a” <s19585574@gmail.com>
2020-10-03 10:21:43 +0700

    add contentful plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,5 +54,12 @@ module.exports = {
         extensions: [`.mdx`, `.md`],
       },
     },
+    {
+      resolve: "gatsby-source-contentful",
+      options: {
+        spaceId: process.env.CONTENTFUL_SPACE_ID,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+      },
+    }
   ],
 }
